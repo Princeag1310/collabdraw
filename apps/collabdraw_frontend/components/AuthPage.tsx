b5943489911ab40843d4ef060a909024d7e6a71b
Prince Agrawal <princeagrawal1013@gmail.com>
2025-07-11 23:22:36 +0530

    correct the signup and signin urls

--- apps/collabdraw_frontend/components/AuthPage.tsx
+++ apps/collabdraw_frontend/components/AuthPage.tsx
@@ -17,7 +17,7 @@ export function AuthPage({ isSignin }: { isSignin: boolean }) {
         setMessage("");
         try {
             if (isSignin) {
-                const res = await axios.post(`${HTTP_BACKEND}/signin`, {
+                const res = await axios.post(`${HTTP_BACKEND}signin`, {
                     username: email,
                     password,
                 });
@@ -25,7 +25,7 @@ export function AuthPage({ isSignin }: { isSignin: boolean }) {
                 localStorage.setItem("token", token);
                 router.push("/rooms");
             } else {
-                await axios.post(`${HTTP_BACKEND}/signup`, {
+                await axios.post(`${HTTP_BACKEND}signup`, {
                     username: email,
                     password,
                     name: email.split("@")[0],
